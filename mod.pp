6d6bfa32f254c2a26992358d5d3377e583649ddd
Cody Bruno <cody@turbot.com>
2025-01-31 14:29:46 -0500

    Update mod metadata

--- mod.pp
+++ mod.pp
@@ -1,16 +1,17 @@
-mod "github_detections" {
+mod "github_audit_log_detections" {
   # hub metadata
-  title         = "GitHub Detections"
-  description   = "Search your GitHub audit logs for high risk actions using Tailpipe."
+  title         = "GitHub Audit Log Detections"
+  description   = "Run detections and view dashboards for your GitHub audit logs to monitor and analyze activity across your GitHub organizations and repositories using Powerpipe and Tailpipe."
   color         = "#191717"
   #documentation = file("./docs/index.md")
-  #icon          = "/images/mods/turbot/github.svg"
-  categories    = ["github", "security"]
+  icon          = "/images/mods/turbot/github-audit-log-detections.svg"
+  categories    = ["dashboard", "detections", "github", "public cloud"]
   database      = var.database
 
   opengraph {
-    title       = "Tailpipe Mod for GitHub Detections"
-    description = "Search your GitHub audit logs for high risk actions using Tailpipe."
-    #image       = "/images/mods/turbot/github-social-graphic.png"
+    title       = "Powerpipe Mod for GitHub Audit Log Detections"
+    description = "Run detections and view dashboards for your GitHub audit logs to monitor and analyze activity across your GitHub organizations and repositories Powerpipe and Tailpipe."
+    image       = "/images/mods/turbot/github-audit-log-detections-social-graphic.png"
   }
+
 }
