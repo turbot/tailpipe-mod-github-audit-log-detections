81b0f11068033021d3fd592c26814ccc9b5920de
misraved <ved@turbot.com>
2025-02-19 19:58:11 +0530

    Fix documentation path for GitHub Audit Log Detections module

--- mod.pp
+++ mod.pp
@@ -3,7 +3,7 @@ mod "github_audit_log_detections" {
   title         = "GitHub Audit Log Detections"
   description   = "Run detections and view dashboards for your GitHub audit logs to monitor and analyze activity across your GitHub organizations and repositories using Powerpipe and Tailpipe."
   color         = "#191717"
-  #documentation = file("./docs/index.md")
+  documentation = file("./docs/index.md")
   icon          = "/images/mods/turbot/github-audit-log-detections.svg"
   categories    = ["dashboard", "detections", "github"]
   database      = var.database
@@ -13,5 +13,4 @@ mod "github_audit_log_detections" {
     description = "Run detections and view dashboards for your GitHub audit logs to monitor and analyze activity across your GitHub organizations and repositories Powerpipe and Tailpipe."
     image       = "/images/mods/turbot/github-audit-log-detections-social-graphic.png"
   }
-
 }
